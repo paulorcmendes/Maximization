49d70a3eb40025a3244b5978ec0d6ee3283709b2
paulorcmendes <mendesdpn@gmail.com>
2017-12-27 18:33:29 -0300

    ploting graphics in 3d

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ POP_SIZE = 100 #POPULATION SIZE
 N_GENERATIONS = 1000 #MAXIMUM NUMBER OF GENERATIONS
 CHRM_SIZE = 64 #CHROMOSSOME SIZE IN BITS
 
-import math as mt
 import numpy as np
 
 #creates a new population
@@ -25,6 +24,24 @@ def Fitness(x):
     return 1/(1+f(x))
 #function f(x)
 def f(x):
-    return -20*mt.exp(-0.2*mt.sqrt((1.0/x.__len__())*sum([xi**2 for xi in x]))) - mt.exp((1.0/x.__len__())*sum([mt.cos(2*mt.pi*xi) for xi in x])) + 20 + mt.exp(1)
+    return -20*np.exp(-0.2*np.sqrt((1.0/x.__len__())*sum([xi**2 for xi in x]))) - np.exp((1.0/x.__len__())*sum([np.cos(2*np.pi*xi) for xi in x])) + 20 + np.exp(1)
 
-print Fitness([0.0,0.0])
+'''
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+fig = plt.figure()
+ax = Axes3D(fig)
+x = np.arange(-100.0, 100.0, 0.1)
+y = x[:]
+x, y = np.meshgrid(x, y)
+z = x[:]+5
+for i in range(0,x.__len__()):
+    for j in range(0,x.__len__()):
+        z[i][j] = Fitness([x[i][j],y[i][j]])
+print z
+print x
+print y
+
+ax.plot_surface(x, y, z, cmap=cm.coolwarm)
+'''
